b95e918162790df522c15911f6503845de1cffc6
leo <lionel126@qq.com>
2021-02-02 15:55:52 +0800

    add config example

---
+++ config.local.example.ts
@@ -0,0 +1,10 @@
+const video1 = '/Users/csg/Movies/Central Intelligence.mp4';
+const video2 = '/Users/csg/Movies/shenteng.mp4';
+export default {
+    // videoFile1: video2,
+    videoFile2: video1,
+    // pic: 'https://cs.xinpianchang.com/uploadfile/article/2020/08/13/5f3530c9ea9b6.jpeg',
+    // upload_web_url: 'https://www-test.xinpianchang.com/upload/index/ts-upload_index?from=tab#',
+    // upload_api: 'https://www-test.xinpianchang.com/index.php?app=upload&ac=index&ts=do',
+
+}
